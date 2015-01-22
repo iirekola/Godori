bb3ab9b9c452de959848686f772a5364bf447ba1
Iiris <iiris.rekola@gmail.com>
2015-01-23 00:35:55 +0200

    pari testiä, nyt on pakko nukkua

--- Godori/src/main/java/godori/Korttipakka.java
+++ Godori/src/main/java/godori/Korttipakka.java
@@ -9,4 +9,14 @@ public class Korttipakka {
     public Korttipakka(ArrayList<Kortti> kortit) {
         this.kortit = kortit;
     }
+    
+    public String toString() {
+        String palautus = "";
+        
+        for (Kortti kortti : kortit) {
+            palautus += kortti;
+        }
+        
+        return palautus;
+    }
 }
